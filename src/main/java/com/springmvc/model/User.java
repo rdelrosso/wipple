7fb30099b2f85b29312472bf22cb659b113bafc2
rdelrosso <roberto.delrosso@gmail.com>
2017-08-24 14:46:05 +0200

    Inserted Hibernate Configs and User Model within its package

--- src/main/java/com/springmvc/model/User.java
+++ src/main/java/com/springmvc/model/User.java
@@ -1,5 +1,39 @@
 package com.springmvc.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(name="name", nullable=true)
+	private String name;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
