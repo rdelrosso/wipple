77a5e5133c918e88cf7e6f077dddb904946bd1bb
rdelrosso <roberto.delrosso@gmail.com>
2017-08-24 12:38:58 +0200

    Inserted project Web configurations and initializer

--- src/main/java/com/springmvc/config/WebConfig.java
+++ src/main/java/com/springmvc/config/WebConfig.java
@@ -1,5 +1,12 @@
 package com.springmvc.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan({"com.springmvc"})
 public class WebConfig {
 
 }
--- src/main/java/com/springmvc/config/WebInitializer.java
+++ src/main/java/com/springmvc/config/WebInitializer.java
@@ -1,5 +1,25 @@
 package com.springmvc.config;
 
-public class WebInitializer {
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+
+		return new Class[]{ WebConfig.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+
+		return new String[]{ "/" };
+	}
 
 }
