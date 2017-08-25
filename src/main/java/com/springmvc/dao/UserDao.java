5a56f1907728f9c45b4fdcc464f1ecde1a289de8
rdelrosso <roberto.delrosso@gmail.com>
2017-08-25 16:47:05 +0200

    Changed Hibernate jar version, added User Dao and UserDaoImpl

--- src/main/java/com/springmvc/dao/UserDao.java
+++ src/main/java/com/springmvc/dao/UserDao.java
@@ -1,5 +1,17 @@
 package com.springmvc.dao;
 
+import java.util.List;
+
+import com.springmvc.model.User;
+
 public interface UserDao {
+	
+	public List<User> getListUser();
+	
+	public void saveOrUpdate(User user);
+	
+	public void deleteUser(int id);
+	
+	public User findUserById(int id);
 
 }
--- src/main/java/com/springmvc/dao/UserDaoImpl.java
+++ src/main/java/com/springmvc/dao/UserDaoImpl.java
@@ -1,5 +1,47 @@
 package com.springmvc.dao;
 
-public class UserDaoImpl {
+import java.util.List;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.springmvc.model.User;
+
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+	
+	protected Session getSession() {
+		return sessionFactory.getCurrentSession();
+	}
+	
+	
+	@SuppressWarnings("unchecked")
+	public List<User> getListUser() {
+		Criteria criteria = getSession().createCriteria(User.class);
+		return (List<User>) criteria.list();
+	}
+
+	public void saveOrUpdate(User user) {
+		getSession().saveOrUpdate(user);
+		
+	}
+
+	public void deleteUser(int id) {
+		User user = (User) getSession().get(User.class, id);
+		getSession().delete(user);
+		
+	}
+
+
+	public User findUserById(int id) {
+		return (User) getSession().get(User.class, id);
+	}
 
 }
